472518e6e7b4ffafc1ce7b1f55bbce3e5188709c
minhasnu <minhas.naheem.in@gmail.com>
2020-11-04 13:00:52 -0800

    prptotypes

---
+++ prototypes.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "utils.h"
+
+
+using namespace std;
+
+//void doSomething();
+
+
+int main(){
+    doSomething();
+    return 0;
+}
+
+void doSomething() {
+    cout << " i am doing something" << endl;
+}
